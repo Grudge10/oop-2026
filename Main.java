12a64f293f437855bf41154171027de8f9f66833
Jan Liam Arias <100700937+Grudge10@users.noreply.github.com>
2026-01-15 04:57:35 +0000

    Created Main.java and used Laptop.java as a class

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Laptop coolShesh = new Laptop();
+        coolShesh.brand = "Asus";
+        coolShesh.storage = 1;
+        coolShesh.memory = 8;
+        coolShesh.dimension = 14;
+        coolShesh.model = "Asus Tuff Gaming 15";
+        coolShesh.condition = "New";
+        coolShesh.price = 56000;
+    }
+}
