3a7b9d0b41bf42efa10977ef4fd0733abe19fff7
Jan Liam Arias <100700937+Grudge10@users.noreply.github.com>
2026-01-15 05:00:34 +0000

    Added in mac as an object and changed coolshesh into asus

--- Main.java
+++ Main.java
@@ -1,12 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        Laptop coolShesh = new Laptop();
-        coolShesh.brand = "Asus";
-        coolShesh.storage = 1;
-        coolShesh.memory = 8;
-        coolShesh.dimension = 14;
-        coolShesh.model = "Asus Tuff Gaming 15";
-        coolShesh.condition = "New";
-        coolShesh.price = 56000;
+        Laptop asus = new Laptop();
+        asus.brand = "Asus";
+        asus.storage = 512;
+        asus.memory = 8;
+        asus.dimension = 14;
+        asus.model = "Asus Tuff Gaming 15";
+        asus.condition = "New";
+        asus.price = 56000;
+
+        Laptop mac = new Laptop();
+        mac.brand = "Apple";
+        mac.storage = 1000;
+        mac.memory = 8;
+        mac.dimension = 14;
+        mac.model = "Macbook Pro mid-2014";
+        mac.condition = "Old";
+        mac.price = 560000;
     }
 }
